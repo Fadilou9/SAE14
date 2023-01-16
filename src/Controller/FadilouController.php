cd43a52a436408a3d913c5cb08143f1ad774a364
Fadilou9 <modoufadiloufall@gmail.com>
2023-01-16 23:01:56 +0100

    html finely

--- src/Controller/FadilouController.php
+++ src/Controller/FadilouController.php
@@ -8,10 +8,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FadilouController extends AbstractController
 {
-    #[Route('/', name: 'templates')]
+    #[Route('/', name: 'app_Fadilou')]
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home.html',[
+        'controller_name' => 'FadilouController',
+    ]);
+            
+    }
+
+     #[Route('/moncv', name: 'app_Fadilou1')]
+    public function cV(): Response
+    {
+        return $this->render('moncv.html',[
+        'controller_name' => 'FadilouController',
+    ]);
+    }
+     #[Route('/formulaire_inscription', name: 'app_Fadilou2')]
+    public function inscription(): Response
+    {
+        return $this->render('formulaire_inscription.html',[
+        'controller_name' => 'FadilouController',
+    ]);
+    }
+     #[Route('/img', name: 'app_Fadilou3')]
+    public function inscriptions(): Response
+    {
+        return $this->render('image.jpg',[
+        'controller_name' => 'FadilouController',
+    ]);
     }
 
 }
