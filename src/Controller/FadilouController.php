d23614be5189a26dc8643c30e28b715bb4f2b899
Fadilou9 <modoufadiloufall@gmail.com>
2023-01-18 11:38:35 +0100

    nouveau repository

--- src/Controller/FadilouController.php
+++ src/Controller/FadilouController.php
@@ -17,14 +17,15 @@ class FadilouController extends AbstractController
             
     }
 
-     #[Route('/moncv', name: 'app_Fadilou1')]
+    #[Route('/moncv', name: 'app_Fadilou1')]
     public function cV(): Response
     {
         return $this->render('moncv.html.twig',[
         'controller_name' => 'FadilouController',
     ]);
     }
-     #[Route('/formulaire_inscription', name: 'app_Fadilou2')]
+   
+    #[Route('/formulaire_inscription', name: 'app_Fadilou2')]
     public function inscription(): Response
     {
         return $this->render('formulaire_inscription.html.twig',[
